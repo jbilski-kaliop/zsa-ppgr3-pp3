2381eeb5930c5dbb369713ed7e515ed5a3aa4285
Jakub Bilski <jakub@blsk.pl>
2026-03-11 10:21:18 +0100

    Simple controller

--- src/classes/App.ts
+++ src/classes/App.ts
@@ -1,5 +1,27 @@
 export default class App {
   public run() {
-    console.log("I'm running!");
+    window.addEventListener("hashchange", () => {
+      console.log(`hash changed to: ${window.location.hash}`);
+
+      this.runAction();
+    });
+
+    if (window.location.hash.length == 0) {
+      window.location.hash = "/";
+    } else {
+      this.runAction();
+    }
+  }
+
+  private runAction() {
+    const path = window.location.hash.replace("#", "");
+
+    if (path === "/") {
+      console.log("Home page");
+    } else if (path === "/contact") {
+      console.log("Contact page");
+    } else {
+      console.log("Error 404");
+    }
   }
 }
